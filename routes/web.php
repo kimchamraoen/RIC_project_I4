a3fcf2e28867e0524ad5b1ad9ae8efbc5ea710ee
kimchamraoen <kimchamraoen@gmail.com>
2025-09-16 16:42:13 +0700

    Route file

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 // use App\Livewire\Crud;
+
+use App\Livewire\Crud;
 use App\Livewire\Login\Form;
 // use App\Livewire\Register;
 // use App\Livewire\Register\Navbar;
@@ -9,6 +11,7 @@ use Livewire\Volt\Volt;
 use App\Livewire\Register\ConductResearch;
 use App\Livewire\Register\ConnectionResearcher;
 
+Route::get('/', Crud::class)->name('home');
 Route::get('/login', Form::class)->name('login');
 Route::get('/conduct', ConductResearch::class)->name('conduct');
 Route::get('/connection', ConnectionResearcher::class)->name('register');
